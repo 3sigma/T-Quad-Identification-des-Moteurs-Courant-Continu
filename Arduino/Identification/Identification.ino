39f1bb68155f0aa932f0bf90209a565947cff18b
Nicolas Gachadoit <Nicolas Gachadoit>
2016-12-15 09:27:35 +0100

    Amélioration diverses compatibles avec MyViz 0.3.2

--- Arduino/Identification/Identification.ino
+++ Arduino/Identification/Identification.ino
@@ -20,6 +20,11 @@
 // https://github.com/wimleers/flexitimer2
 #include <FlexiTimer2.h>
 
+// Inclusion d'une bibliothèque permettant de gérer l'écran OLED.
+// Télécharger à l'adresse http://www.3sigma.fr/telechargements/U8glib.zip
+// et décompresser dans le sous-répertoire « libraries » de votre installation Arduino
+#include "U8glib.h"
+
 // Taille des tableaux de mesure
 #define arraysize 150
 
@@ -49,12 +54,34 @@ long compteurMesureCourant = 0L;
 
 float Vref = 5.;
 float dt = 0.01;
+float VBat;
 
 // Résistances du circuit d'amplification de courant
 float R1 = 1000.;
 float R2 = 10000.;
 // Résistance de mesure
 float Rmes = 0.1;
+
+// Déclaration de l'objet écran OLED
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_DEV_0|U8G_I2C_OPT_NO_ACK|U8G_I2C_OPT_FAST);  // Fast I2C / TWI 
+
+// Fonction d'affichage sur l'écran
+void draw(void) {
+  // Définition de la font
+  u8g.setFont(u8g_font_unifont);
+
+  // Affichage d'une estimation de la tension d'alimentation des moteurs au cours de l'essai
+  u8g.setPrintPos(0, 10);
+  u8g.print("Tension d'alim");
+  u8g.setPrintPos(0, 25);
+  u8g.print("pour essai");
+  u8g.setPrintPos(0, 40);
+  u8g.print("en courant: ");
+  u8g.setPrintPos(0, 55); 
+  String strVAlim = String(VBat,2);
+  strVAlim += " V";
+  u8g.print(strVAlim);
+}
 
 void setup() {
   Serial.begin(115200);
@@ -68,11 +95,11 @@ void setup() {
   pinMode(pwmMoteurArriereGauche, OUTPUT);
   pinMode(mesurePWM_MoteurArriereGauche, INPUT);
 
-  // Modification du prescaler de l'ADC: division par 16 de la fréquence du micro-contrôleur
+  // Modification du prescaler de l'ADC: division par 8 de la fréquence du micro-contrôleur
   // au lieu de 128 par défaut, pour diminuer le temps entre deux mesures analogiques
-  bitSet(ADCSRA, ADPS2);
-  bitClear(ADCSRA, ADPS1);
-  bitClear(ADCSRA, ADPS0);
+  bitClear(ADCSRA, ADPS2);
+  bitSet(ADCSRA, ADPS1);
+  bitSet(ADCSRA, ADPS0);
 
   FlexiTimer2::set(1, 1/10000., isrt); // résolution timer = 0.1 ms
   FlexiTimer2::start();
@@ -82,14 +109,16 @@ void setup() {
 }
 
 void loop() {
+  // Première phase: mesure du courant alors que le moteur
+  // ne tourne pas encore
   if ((nbData > (arraysize/2)-1) && (over == 0)) {
     over = 1;
     
-    // Stop the motors
+    // Arrêt des moteurs
     digitalWrite(pwmMoteurArriereGauche, LOW);
-    // Arrêt de l'interruption Timer 2
-    //FlexiTimer2::stop();
   }
+  // Fin de la première phase: transmission des mesures de courant
+  // via la liaison série
   else if (over == 1) {
     over = 2;
 
@@ -105,35 +134,39 @@ void loop() {
       Serial.print(",");
       Serial.println((float)omega[i], 3);
     }
-    
-    // A chaque changement de niveau de tension sur le pin A du codeur,
+
+    // Activation d'une interruption pour que, à chaque changement de niveau de tension sur le pin A du codeur,
     // on exécute la fonction GestionInterruptioncodeurArriereGauchePinA (définie à la fin du programme)
     enableInterrupt(codeurArriereGauchePinA, GestionInterruptioncodeurArriereGauchePinA, CHANGE);
   
     // Calcul courant moteur, sur interruption PWM
     enableInterrupt(mesurePWM_MoteurArriereGauche, calculCourantMoteur, RISING);
 
-    // Redémarrage du Timer 2 pour un échantillonnage moins rapide
-    // (pour la mesure de la vitesse du moteur)
+    // Reinitialisations pour la phase suivante
     delay(100);
     ticksCodeur = 0;
-    //FlexiTimer2::set(10, 1/1000., isrt); // résolution timer = 0.1 ms
-    //FlexiTimer2::start();
     over = 3;
 
-    // Consigne de vitesse moteur (correspond à environ 6V)
-    analogWrite(pwmMoteurArriereGauche, 195);    
+    // Lecture de la tension batterie
+    VBat = 0.;
+    // La première lecture peut être fausse
+    while (VBat < 6.) {
+      VBat = 3. * (Vref * (float)analogRead(5) / 1024.);
+    }
+
+    // Seconde phase: mesure de la vitesse du moteur et du courant
+    // pendant la montée en vitesse
+    // Consigne de vitesse moteur à 6V
+    analogWrite(pwmMoteurArriereGauche, (uint8_t)(1530/VBat)); // 1530 = 6 * 255
   }
+  // Fin de la seconde phase: transmission des vitesses et mesures de courant
+  // via la liaison série
   else if ((over == 3) && (nbData > arraysize-2)) {
     over = 4;
     // Arrêt du moteur
     analogWrite(pwmMoteurArriereGauche, 0);
     
-//    Serial.println("nbData: ");
-//    Serial.println(nbData);
-//    Serial.println("im: ");
-    // On met arbitrairement le dernier point à 0 (ou plutôt à 512,
-    // qui correspond au 0 physique) pour que l'identification
+    // On met arbitrairement les dernier points à 0 pour que l'identification
     // puisse se faire correctement dans MyViz
     courant[nbData - 1] = 0.0;
     omega[nbData - 1] = 0;
@@ -150,31 +183,43 @@ void loop() {
     FlexiTimer2::stop();
 
   }
+  else if (over == 4) {
+    // Afichage sur l'écran OLED
+    delay(500);
+    u8g.firstPage();  
+    do {
+      draw();
+    } while( u8g.nextPage() );
+  }
 }
 
-/*** Interrupt routine ***/
+// Routine exécutéee sur interruption Timer 2
 void isrt() {
-
+  // Première phase
   if (over == 0) {
-    // Compte-tenu de la structure du driver moteur utilisé,
-    // le courant qui passe dans un moteur est la somme des deux courants mesurés
-    adc = analogRead(mesureCourant_MoteurArriereGauche);// + analogRead(mesureCourant_MoteurArriereDroit);
+    // Mesure du courant et stockage dans un tableau
+    // Compte-tenu de la structure du driver de puissance utilisé, le courant est la somme
+    // de celui passant dans les résistances arrière droite et arrière gauche
+    adc = analogRead(mesureCourant_MoteurArriereGauche) + analogRead(mesureCourant_MoteurArriereDroit);
     tval[nbData] = micros();
     courant[nbData] = (Vref * ((float)adc)/1024.) / ((1 + R2/R1) * Rmes);
     nbData++;
   }
+  // Seconde phase
   else if (over == 3) {
-    // Pour la vitesse, une exécution sur 100 (échantillonage à 10 ms au lieu de 100 µs)
-    adc = analogRead(mesureCourant_MoteurArriereGauche);// + analogRead(mesureCourant_MoteurArriereDroit);
+    // Compte-tenu de la structure du driver de puissance utilisé, le courant est la somme
+    // de celui passant dans les résistances arrière droite et arrière gauche
+    adc = analogRead(mesureCourant_MoteurArriereGauche) + analogRead(mesureCourant_MoteurArriereDroit);
     courantsomme += (Vref * ((float)adc)/1024.) / ((1 + R2/R1) * Rmes);
     compteurMesureCourant++;
+    // Pour la vitesse, une exécution sur 100 interruptions Timer 2 (échantillonage à 10 ms au lieu de 100 µs)
     if (idecim == 100) {
       idecim = 0;
       tval[nbData] = micros();
       
       // Nombre de ticks codeur depuis la dernière fois: correspond à l'angle de rotation du moteur pendant la période d'échantillonnage
-      // Ce nombre est mis à jour par les fonctions GestionInterruptioncodeurArriereGauchePinA et GestionInterruptioncodeurArriereGauchePinB,
-      // exécutées à chaque interruption due à une impulsion sur la voie A ou B du codeur incrémental
+      // Ce nombre est mis à jour par la fonction GestionInterruptioncodeurArriereGauchePinA
+      // exécutées à chaque interruption due à une impulsion sur la voie A du codeur incrémental
       omega[nbData] = ticksCodeur;
   
       // Une fois lu, ce nombre est remis à 0 pour pouvoir l'incrémenter de nouveau sans risquer de débordement de variable
@@ -191,7 +236,7 @@ void isrt() {
 }
 
 void calculCourantMoteur() {
-  // Calcul du courant moyen sur la période PWM haut / PWM bas écoulée. Il y a donc un décalage de 2 ms
+  // Calcul du courant moyen sur la période PWM haut / PWM bas écoulée.
   if (compteurMesureCourant > 0) {
     courant[nbData] = courantsomme / (float)compteurMesureCourant;
     compteurMesureCourant = 0L;
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # T-Quad - Identification des Moteurs à Courant Continu
-Programmes et appplications d'identification des moteurs à courant continu du robot T-Quad
+Programmes et appplications d'identification des moteurs à courant continu du robot T-Quad.
+
+Attention: l'utilisation des programmes Arduino nécessite l'installation de bibliothèques complémentaires, comme indiqué dans la [documentation générale](https://github.com/3sigma/T-Quad/raw/master/T-Quad_General.pdf).
